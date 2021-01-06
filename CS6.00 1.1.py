0052960171c3343b7b0fdbe9dd0afb0e5af4d703
Margaret Landefeld <mlandefeld@gmail.com>
2021-01-05 22:28:10 -0500

    Problem 1.1

---
+++ CS6.00 1.1.py
@@ -0,0 +1,20 @@
+#Problem Set 1 #1
+#Computes 1000th prime number
+currentNumber = 3
+primes = [2]
+n = 1000
+
+while(len(primes) < n) :
+    isPrime = False
+    for prime in primes :
+        #checks that the current number is not divisible by the current prime number
+        if currentNumber % prime != 0 :
+            isPrime = True
+        else :
+            #current is definitely composite so we move on
+            isPrime = False
+            break
+    if isPrime :
+        primes.append(currentNumber)
+    currentNumber += 1
+print(primes[-1])
