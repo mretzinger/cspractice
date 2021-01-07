955b8dcd8ab2190a0bccbb1d8e62a94f02e577f8
Margaret Landefeld <mlandefeld@gmail.com>
2021-01-07 17:26:27 -0500

    Problem 2 progress

---
+++ CS6.00 1.2.py
@@ -0,0 +1,27 @@
+import math
+#Problem Set 1 #2
+#Computes 1000th prime number
+currentNumber = 3
+primes = [2]
+sumLogs = 0
+n = 20
+
+while(len(primes) < n) :
+    isPrime = False
+    for prime in primes :
+        #checks that the current number is not divisible by the current prime number
+        if currentNumber % prime != 0 :
+            isPrime = True
+        else :
+            #current is definitely composite so we move on
+            isPrime = False
+            break
+    if isPrime :
+        primes.append(currentNumber)
+    currentNumber += 1
+
+for prime in primes :
+    sumLogs += math.log(prime)
+print('Sum of the logs of the primes: ',sumLogs)
+print('n: ', n)
+print('ratio: ', (sumLogs/n))
