3d35ade466b1c763e480b1a156a9e9e0f04d344b
Margaret Landefeld <mlandefeld@gmail.com>
2021-02-02 22:29:21 -0500

    Problem set 2 answer 1 and notes

--- CS6.00 2.3.py
+++ CS6.00 2.3.py
@@ -4,11 +4,43 @@
 #Time: 
 
 
+# def nuggets(n) :
+#     for a in range(1,100) : 
+#         for b in range(1,100) :
+#             for c in range(1,100) :
+#                 n = 6*a + 9*b +20*c
+#                 print(n)
+
+
 def nuggets(n) :
-    for a in range(1,150) : 
-        for b in range(1,150) :
-            for c in range(1,150) :
-                n = 6*a + 9*b +20*c
+    a = 0
+    b = 0
+    c = 1
+    # if n == 6*a + 9*b +20*c :
+    #     print(n, ' : ', a, b, c)
+    # for a in range(0, 10) :
+    #     if n == 6*a + 9*b +20*c :
+    #         print(n, ' : ', a, b, c)
+    #         for b in range(0,10) :
+    #             if n == 6*a + 9*b +20*c :
+    #                 print(n, ' : ', a, b, c)
+    #                 for c in range(0,10) :
+    #                     if n == 6*a + 9*b +20*c :
+    #                         print(n, ' : ', a, b, c)
+
+    for a in range(0,10) :
+        #if n == 6*a + 9*b +20*c :
+        #print('test', b) #n, ' : ', a, b, c)
+        for b in range(0,10) :
+            if n == 6*a + 9*b + 20*c :
+                print('works') #n, ' : ', a, b, c)
+            if n == 6*a + 9*b +20*c  : 
+                print('success', a, b, 6*a + 9*b + 20*c)
+
+    # for a in range(1,100) : 
+    #     for b in range(1,100) :
+    #         for c in range(1,100) :
+                
 
 
 print(nuggets(50))
@@ -19,9 +51,5 @@ nList = []
 # while nList.len() <= 6 :
 #     n = 6*a + 9*b +20*c
 
-
-
-
-
-
 #print('Largest number of McNuggets that cannot be bought in exact quantity: ', n)
+
