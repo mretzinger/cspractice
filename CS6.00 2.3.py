4559f0b048a2c88c46663441a11fcb4862fdcf5b
Margaret Retzinger <maretzinger@gmail.com>
2021-02-25 20:24:34 -0500

    problem set 2.2 notes

--- CS6.00 2.3.py
+++ CS6.00 2.3.py
@@ -1,8 +1,21 @@
-#Problem Set 2 (Part I)
+#Problem Set 2 (Part 3)
 #Name: Margaret Retzinger
-#Collaborators: 
+#Collaborators: Damien Retzinger
 #Time: 
 
+#“Largest number of McNuggets that cannot be bought in exact quantity: <n>” 
+
+# Hypothesize possible instances of numbers of McNuggets that cannot be purchased
+# exactly, starting with 1
+# For each possible instance, called n,
+# Test if there exists non-negative integers a, b, and c, such that
+# 6a+9b+20c = n. (This can be done by looking at all feasible
+# combinations of a, b, and c)
+# If not, n cannot be bought in exact quantity, save n
+# When you have found six consecutive values of n that in fact pass the test of
+# having an exact solution, the last answer that was saved (not the last value
+# of n that had a solution) is the correct answer, since you know by the
+# theorem that any amount larger can also be bought in exact quantity 
 
 # def nuggets(n) :
 #     for a in range(1,100) : 
@@ -12,12 +25,14 @@
 #                 print(n)
 
 
-def nuggets(n) :
+def nuggets() :
     a = 0
     b = 0
-    c = 1
-    # if n == 6*a + 9*b +20*c :
-    #     print(n, ' : ', a, b, c)
+    c = 0
+    n=1
+    while n < 60 : 
+        if n == 6*a + 9*b +20*c :
+            print(n, ' : ', a, b, c)
     # for a in range(0, 10) :
     #     if n == 6*a + 9*b +20*c :
     #         print(n, ' : ', a, b, c)
@@ -36,6 +51,7 @@ def nuggets(n) :
                 print('works') #n, ' : ', a, b, c)
             if n == 6*a + 9*b +20*c  : 
                 print('success', a, b, 6*a + 9*b + 20*c)
+    print('Largest number of McNuggets that cannot be bought in exact quantity: ', n)
 
     # for a in range(1,100) : 
     #     for b in range(1,100) :
@@ -43,13 +59,12 @@ def nuggets(n) :
                 
 
 
-print(nuggets(50))
+print(nuggets())
 
-n = 1
-nList = []
+# n = 1
+# nList = []
 
 # while nList.len() <= 6 :
 #     n = 6*a + 9*b +20*c
 
-#print('Largest number of McNuggets that cannot be bought in exact quantity: ', n)
 
